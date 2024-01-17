086fb5669d04cf9f76c360a49e9f16f012a6cff1
Mike Marshall <109046283+mike-j-marshall@users.noreply.github.com>
2024-01-17 15:04:18 -0700

    Add files via upload

---
+++ 1-17-24/code.js
@@ -0,0 +1,18 @@
+// This code does not execute properly. What is the solution?
+// function multiply (a,b){
+//    a * b
+//}
+
+//P: The function will take in numbers to be multiplied.
+
+//R: The original code does not specify what will be returned. It should return the product of the numbers multiplied.
+
+//E: If we are given the numbers 7 and 4, the function shoulder return the product of 28.
+
+//P: Create a function that returns the product of two numbers.
+
+multiply = function (a,b){
+    return a * b;
+}
+
+console.log(function(7,4))
