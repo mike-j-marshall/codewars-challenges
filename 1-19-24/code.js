9271005b8e67f63da6f86636c1ef9d74a2475c38
Mike Marshall <109046283+mike-j-marshall@users.noreply.github.com>
2024-01-19 16:19:55 -0700

    Add files via upload

---
+++ 1-19-24/code.js
@@ -0,0 +1,13 @@
+// Declare a variable and assign it to a sentence as a string. Alert if the sentence is a question.
+
+//P: Assign a variable to a string that either does or does not include a question mark.
+
+//R: Return an alert if the string is a question.
+
+//E: If the string ends with '?' an alert should trigger.
+
+//P: Provide a string that ends with a question mark. An alert is provided.
+
+const str = 'Will the Avalanche win another Stanley Cup this year?'
+
+alert(str.endsWith('?'))
