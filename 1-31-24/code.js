613eaf5a03d6fcb2ebfe46f2fd52a72cb76282ce
Mike Marshall <109046283+mike-j-marshall@users.noreply.github.com>
2024-01-31 18:52:26 -0700

    Add files via upload

---
+++ 1-31-24/code.js
@@ -0,0 +1,11 @@
+// Create a function that takes a string. Print the reverse of that string to the console.
+
+//P: The string will be passed through three different methods including split, reverse, and join.
+
+//R: The string will be returned corresponding to its method.
+
+//E: The function takes in the word 'confidence', which is then split up into individual charcters, those characters are reversed, and finally re-joined as 'ecnedifnoc'.
+
+//P: Create a function that takes in a string. That string is then split, reversed, and joined to be printed to the console in reverse.
+
+let unoReverse = str => console.log (str.split('').reverse().join(''))
