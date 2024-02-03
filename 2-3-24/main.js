4028243f3b1f18542444825e407526103701e8c4
Mike Marshall <109046283+mike-j-marshall@users.noreply.github.com>
2024-02-03 16:38:03 -0700

    Add files via upload

---
+++ 2-3-24/main.js
@@ -0,0 +1,13 @@
+// What is a typical "for" loop?
+
+//P: The paramaters for a for loop take in "initialization", a "condition", and an "afterthought".
+
+//R: The return statement terminates the loop and the entire function.
+
+//E: A "for" loop repeats until a specified condition evaluates to false.
+
+//P: An initializing expression (if any), is executed. The condition expression is evaluated. The statement executes.
+
+for (let i = 1; i < 5; i++){
+    console.log(i)
+}
