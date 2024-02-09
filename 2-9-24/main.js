da7612879d2c5669b2407a21a724efdd19cd6f46
Mike Marshall <109046283+mike-j-marshall@users.noreply.github.com>
2024-02-09 14:42:55 -0700

    Add files via upload

---
+++ 2-9-24/main.js
@@ -0,0 +1,13 @@
+// Create a function that can transform a number into a string.
+
+//P: Insert any number as a parameter to be converted into a string.
+
+//R: The number will be returned as a string spelled out with its corresponding letters.
+
+//E: If the number 17 is inserted into the function, its string of 'seventeen' will be returned.
+
+//P: A function is created with the purpose of taking in a number to be converted and returned as a string.
+
+function numberToString(num){
+    return num.toString();
+}
