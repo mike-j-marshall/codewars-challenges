76b5eda997cb8d55fa814e1179999b7fc53b98f9
Mike Marshall <109046283+mike-j-marshall@users.noreply.github.com>
2024-02-10 15:26:42 -0700

    Add files via upload

---
+++ 2-10-24/main.js
@@ -0,0 +1,13 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+//P: The function will take in boolean parameter.
+
+//R: A string of either "Yes" or "No" will be returned depending on whether a boolean is taken in.
+
+//E: If a boolean value is entered, it will be returned as either "Yes" for true, or "No" for false.
+
+//P: A function is created to utilize the method that takes in a boolean value. "Yes" is returned for true and "No" is returned for false.
+
+function boolToWord(bool){
+    return bool ? 'Yes' : 'No';
+}
