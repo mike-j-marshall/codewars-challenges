1d6deb4cd41db7d3a07e07b6639adb1816cf6145
Mike Marshall <109046283+mike-j-marshall@users.noreply.github.com>
2024-02-11 14:28:19 -0700

    Add files via upload

---
+++ 2-11-24/main.js
@@ -0,0 +1,14 @@
+// You are given an integer or a floating-point number, find its opposite.
+
+//P: An integer or floating-point number is taken in as a parameter.
+
+//R: The integer or floating-point number will be returned as its opposite.
+
+//E: If the number 29.8 is taken in as a floating-point number, it will be returned as its opposite, which is -29.8.
+
+//P: Create a function that takes in an integer or floating-point number. That number is then returned as its opposite.
+
+function opposite (num){
+    return (-number);
+}
+console.log(opposite(29.8))
